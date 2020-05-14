fc18e74876efc02960d1ac065f52f928767f8397
danisty <57642783+danisty@users.noreply.github.com>
2020-05-14 15:47:43 +0200

    Create tools.lua

---
+++ noname/tools.lua
@@ -0,0 +1,11 @@
+local dir = 'https://github.com/danisty/legostuff/noname/'
+return {
+	{
+		Name = 'Remote Spy'
+		Directory = dir .. 'RemoteSpy'
+	},
+	{
+		Name = 'Environment Editor'
+		Directory = dir .. 'EnvironmentEditor'
+	}
+}
